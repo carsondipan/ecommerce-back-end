b72de8e5896b6c46e2004f92737937b4e4efe6e3
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-01-25 10:43:02 -0500

    getting into creating the data models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,21 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    product_price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      
+    }
   },
   {
     sequelize,
